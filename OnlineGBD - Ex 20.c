044c078233ccf45d286c96b8d9a33fd9d59ce5d0
Guilherme Wilias <113528431+guilhermewilias@users.noreply.github.com>
2023-06-01 15:47:48 -0300

    Add files via upload

---
+++ OnlineGBD - Ex 20.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (){
+	
+	int num[5];
+	
+	printf ("Entre com 5 numeros inteiros ", num[5]);
+	scanf ("%d",num[5]);
+}
---
+++ OnlineGBD - Ex 23.c
@@ -0,0 +1,14 @@
+#include <Stdio.h>
+
+void Item_01();
+void Item_02();
+void Item_03();
+void Item_04();
+
+int main() {
+Item_01();
+Item_02();
+Item_03();
+Item_04();
+return 0;
+}
